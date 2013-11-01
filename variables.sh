a48e0f89a49d25fe016f5acfde1da3144778748d
Jesse Patching <jesse@patching.ca>
2013-11-01 08:24:09 -0600

    Version bump

--- variables.sh
+++ variables.sh
@@ -4,7 +4,7 @@ APACHE_APR_VERSION="1.4.8"    # http://apr.apache.org/download.cgi
 APACHE_APR_URL="http://mirror.csclub.uwaterloo.ca/apache/apr/apr-${APACHE_APR_VERSION}.tar.gz"
 APACHE_APR_UTIL_VERSION="1.5.2"    # http://httpd.apache.org/download.cgi
 APACHE_APR_UTIL_URL="http://mirror.csclub.uwaterloo.ca/apache/apr/apr-util-${APACHE_APR_UTIL_VERSION}.tar.gz"
-APACHE_MOD_FCGID_VERSION="2.3.7"
+APACHE_MOD_FCGID_VERSION="2.3.9"
 APACHE_MOD_FCGID_URL="http://apache.sunsite.ualberta.ca//httpd/mod_fcgid/mod_fcgid-${APACHE_MOD_FCGID_VERSION}.tar.gz"
 APACHE_MOD_MACRO_VERSION="1.2.1"    # http://people.apache.org/~fabien/mod_macro/
 APACHE_MOD_MACRO_URL="http://people.apache.org/~fabien/mod_macro/mod_macro-${APACHE_MOD_MACRO_VERSION}.tar.gz"
@@ -18,7 +18,7 @@ ANT_CONTRIB_VERSION="1.0b3"    # http://ant-contrib.sourceforge.net/
 ANT_CONTRIB_URL="http://sourceforge.net/projects/ant-contrib/files/ant-contrib/${ANT_CONTRIB_VERSION}/ant-contrib-${ANT_CONTRIB_VERSION}-bin.tar.gz/download"
 ANT_TGZ_FILE="ant-${ANT_VERSION}.tar.gz"
 
-PHP_VERSION="5.4.19"    # http://php.net/downloads.php
+PHP_VERSION="5.4.21"    # http://php.net/downloads.php
 PHP_URL="http://us1.php.net/get/php-${PHP_VERSION}.tar.gz/from/us1.php.net/mirror"
 LIBMCRYPT_VERSION="2.5.8"    # http://sourceforge.net/projects/mcrypt/files/Libmcrypt/
 LIBMCRYPT_URL="http://sourceforge.net/projects/mcrypt/files/Libmcrypt/${LIBMCRYPT_VERSION}/libmcrypt-${LIBMCRYPT_VERSION}.tar.bz2/download"
@@ -30,7 +30,7 @@ PHP_TGZ_FILE="php-${PHP_VERSION}.tar.gz"
 
 COMPOSER_URL="https://getcomposer.org/installer"
 
-NEWRELIC_VERSION="3.9.5.13"    # http://download.newrelic.com/php_agent/release/
+NEWRELIC_VERSION="4.1.5.24"    # http://download.newrelic.com/php_agent/release/
 NEWRELIC_URL="http://download.newrelic.com/php_agent/archive/${NEWRELIC_VERSION}/newrelic-php5-${NEWRELIC_VERSION}-linux.tar.gz"
 NEWRELIC_TGZ_FILE="newrelic-${NEWRELIC_VERSION}.tar.gz"
 
